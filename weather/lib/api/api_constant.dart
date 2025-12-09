416a5c0b77204a2bebd421618c60cc823ed581d2
lil3le <aliobeidat266@gmail.com>
2025-12-09 14:33:01 +0300

    Create api_constant.dart

---
+++ weather/lib/api/api_constant.dart
@@ -0,0 +1,7 @@
+class ApiConstant {
+  static const String baseUrl = 'https://api.openweathermap.org/data/2.5';
+  static const String apiKey = '3806e737747e893b99817a6da13c337f';
+  
+  static const String weatherEndpoint = '/weather';
+  static const String forecastEndpoint = '/forecast';
+}
